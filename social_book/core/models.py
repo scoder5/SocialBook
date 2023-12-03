70bac278409f0c38fe0c3f7ac1ae50d1f914c05a
Surya <scoding83@gmail.com>
2023-12-03 12:09:11 +0530

    Models Updated

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField()
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.TextField()
+
+    def __str__(self) -> str:
+        return self.user.username
